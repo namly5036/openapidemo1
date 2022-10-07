041e885ab1c68c1a018b8e5c5a844b97a53469eb
KEVIN-LEE\Admin <nam.ly@propzy.com>
2022-10-07 17:24:18 +0700

    add source

---
+++ src/main/java/com/openapi/demo1/OpenapiDemo1Application.java
@@ -0,0 +1,13 @@
+package com.openapi.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/openapi/demo1/OpenapiDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
